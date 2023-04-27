7d470551c94222e4fbadb9963bc19ee6a47d31db
Mateus Abdallah <abdallahmateus@gmail.com>
2023-04-27 06:39:20 -0700

    set constructor and a deposit method

--- bankSystemSoftware/BankAccount.java
+++ bankSystemSoftware/BankAccount.java
@@ -2,4 +2,21 @@ package bankSystemSoftware;
 
 public class BankAccount {
 
+	double balance;
+	double newBalance;
+	String customerName;
+	String customerId;
+	
+	public BankAccount(String customerName, String customerId) {
+		
+		this.customerName = customerId;
+		this.customerId = customerId;
+	}
+	
+	void deposit(double amount) {
+		if(amount != 0) {
+			bal += amount;
+			newBalance = amount;
+		}
+	}
 }
